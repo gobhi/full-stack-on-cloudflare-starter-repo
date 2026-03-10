465a932a93e15434dac2873edb84aae2da534dc1
Gobhi Theivendran <gobhit@gmail.com>
2026-03-10 12:29:28 -0400

    wip

--- apps/data-service/src/hono/app.ts
+++ apps/data-service/src/hono/app.ts
@@ -1,11 +1,26 @@
 import { Hono } from 'hono';
 import { getLink } from '@repo/data-ops/queries/links';
-
+import { cloudflareInfoSchema } from '@repo/data-ops/zod-schema/links';
+import { getDestinationForCountry } from '../helpers/route-ops';
 export const App = new Hono<{ Bindings: Env }>();
 
 
+
 App.get('/:id', async (c) => {
-  const id = c.req.param('id')
-  const linkInfoFromDb = await getLink(id)
-  return c.json(linkInfoFromDb)
+  const id = c.req.param('id');
+
+  const linkInfo = await getLink(id)
+  if (!linkInfo) {
+    return c.text('Destination not found', 404);
+  }
+
+  const cfHeader = cloudflareInfoSchema.safeParse(c.req.raw.cf)
+  if (!cfHeader.success) {
+    return c.text('Invalid Cloudflare headers', 400);
+  }
+
+  const headers = cfHeader.data
+  console.log(headers)
+  const destination = getDestinationForCountry(linkInfo, headers.country)
+  return c.redirect(destination)
 })
